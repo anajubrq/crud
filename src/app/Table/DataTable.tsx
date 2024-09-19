3f3bca3a9bb4cb6f99c5bfbaa0841906b9510436
Ana Julia <anajuliabrq2@gmail.com>
2024-09-19 12:08:46 -0300

    Adicionei Modal do botão Delete

--- src/app/Table/DataTable.tsx
+++ src/app/Table/DataTable.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { ButtonDelete } from "../Buttons/ButtonDelete";
-//ort { Button } from "@/components/ui/button";
+import { ModalDelete } from "../Buttons/ModalDelete";
 
 export interface Post {
   id: number;
@@ -15,13 +15,13 @@ export interface DataTableProps {
 }
 
 export function DataTable({ posts, deletePost }: DataTableProps) {
- 
+  const [openDelete, setOpenDelete] = useState<boolean>(false);
+
   return (
-    
     <section className="mt-8 z-0">
-      <Table className="border-2 border-gray-400 rounded-lg w-full">
+      <Table className="border-2 border-gray-400 rounded-lg w-full ">
         <TableCaption className="text-[18px]">Posts recentes</TableCaption>
-        <TableHeader className="bg-slate-300 ">
+        <TableHeader className="bg-slate-300 rz">
           <TableRow>
             <TableHead className="text-[16px] px-4 py-2 h-9 uppercase flex flex-row justify-between">
               <p>Título</p>
@@ -29,9 +29,9 @@ export function DataTable({ posts, deletePost }: DataTableProps) {
             </TableHead>
           </TableRow>
         </TableHeader>
-        <TableBody className="w-screen">
+        <TableBody>
           {posts.map((post) => (
-            <TableRow key={post.id} className="border-b border-gray-300 flex justify-between w-[1000px] ">
+            <TableRow key={post.id} className="border-b border-gray-300 flex justify-between w-full">
               <TableCell className="px-4 py-2">
                 <div className="flex flex-col">
                   <span className="text-[18px] font-bold">{post.title}</span>
@@ -39,11 +39,13 @@ export function DataTable({ posts, deletePost }: DataTableProps) {
                 </div>
               </TableCell>
               <TableCell className="px-4 py-2 text-right">
-                <ButtonDelete onClick={() => deletePost(post.id)} />
+                <button onClick={() => setOpenDelete(!openDelete)}>  <img src="/images/delete.png" className="w-[25px]" alt="Delete" /></button>
+                <ModalDelete
+                  isOpenDelete={openDelete}
+                  setOpenDelete={setOpenDelete}
+                  deletePost={() => deletePost(post.id)} 
+                />
               </TableCell>
-             
-             
-          
             </TableRow>
           ))}
         </TableBody>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,20 @@
-
-"use client";
-import React, {  useState } from "react";
+"use client"
+import React, { useState } from "react";
 import DataTable, { Post } from "./Table/DataTable";
 import Header from "./Header/Header";
 
 export default function Home() {
   const [allPosts, setAllPosts] = useState<Post[]>([]);
-  const deletePost = (id: number) => {
-    console.log("Post deletado:", id);
-    setAllPosts((prevPosts) => prevPosts.filter((post) => post.id !== id));
- 
 
+  const deletePost = (id: number) => {
+    console.log("Post Deletado:", id);
+    setAllPosts((prevPosts) => prevPosts.filter((post) => post.id !== id));
   };
-  
+
   return (
     <section>
-      <div>
-        <Header setAllPosts={setAllPosts} />
-      </div>
-      <div>
-        <DataTable
-         posts={allPosts}
-         deletePost={deletePost}
-         
-          />
-      </div>
+      <Header setAllPosts={setAllPosts} />
+      <DataTable posts={allPosts} deletePost={deletePost} />
     </section>
   );
 }
